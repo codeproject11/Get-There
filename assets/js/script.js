b6ff05b278bd3af56ddf09cebff8e47721044fba
TaimurHasan <taimurhasan11@gmail.com>
2022-03-30 19:29:05 -0400

    adding base html structure and skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// store user input from form
+// store main div or section in variable to use in appending
+
+// fetch apis (multiple)
+
+// display functions (1 for flights, 1 for weather, 1 for hotels/restaurants)

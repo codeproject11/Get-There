6a3651db7633e9d786b70617ba092dd8e54a751b
charanvir <charanvir123@gmail.com>
2022-03-31 01:54:56 -0400

    changed HTML to allow user to input current and destination city
    further expanded the getCity function to include currentCity information
    
     created function (and commented out to save api pull requests) to fetch data from an API that shows attractions in the user inputted destination city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 // variable to access main search form in js (to apply submit event listener to);
 var inputForm = document.querySelector('#search-form');
-var userInput = document.querySelector("#city");
+var destinationInput = document.querySelector("#destinationCity");
+var currentInput = document.querySelector("#currentCity");
+// openweather API = f9dcdf6690d0d22c5198371e258e8bb2
+// opentrip API = 5ae2e3f221c38a28845f05b69efdf761d15a073a57889029b8209b98
 
 var storeInput = function (event) {
     event.preventDefault();
-    var input = document.querySelector("input[id='city']").value;
-    if (input) {
-        userInput.value = "";
-        console.log(input);
-        getCity(input);
+    var destinationCity = document.querySelector("input[id='destinationCity']").value;
+    var currentCity = document.querySelector("input[id='currentCity']").value;
+    if (destinationCity && currentCity) {
+        destinationInput.value = "";
+        currentInput.value = "";
+        getCity(destinationCity, currentCity);
     } else {
         alert("Please enter a city name")
     }
@@ -19,16 +23,37 @@ inputForm.addEventListener("submit", storeInput);
 // store main div or section in variable to use in appending
 
 // fetch apis (multiple)
-var getCity = function (cityName) {
-    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=f9dcdf6690d0d22c5198371e258e8bb2";
-    fetch(apiURL)
+// function to use user inputted destination city and current city and generate data from API
+var getCity = function (destinationCity, currentCity) {
+    var destinationCityAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + destinationCity + "&limit=1&appid=f9dcdf6690d0d22c5198371e258e8bb2";
+    var currenCityAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + currentCity + "&limit=1&appid=f9dcdf6690d0d22c5198371e258e8bb2";
+    fetch(destinationCityAPIURL)
         .then(function (res) {
             if (res.ok) {
                 res.json().then(function (data) {
                     if (data.length === 0) {
                         alert("Please enter a valid city name")
                     } else {
-                        destinationCityWeather(data[0].lat, data[0].lon, data[0].name)
+                        weatherData(data[0].lat, data[0].lon, data[0].name)
+                        // attractions(data[0].lon, data[0].lat)
+                    };
+                });
+            };
+        })
+        .catch(function (error) {
+            alert("Please Try Again");
+            console.log(error);
+            return
+        });
+    fetch(currenCityAPIURL)
+        .then(function (res) {
+            if (res.ok) {
+                res.json().then(function (data) {
+                    if (data.length === 0) {
+                        alert("Please enter a valid city name")
+                    } else {
+                        console.log(`Current City: ${data[0].name}`)
+                        console.log(`Current City Coordinates: Lat- ${data[0].lat}, Lon- ${data[0].lon}`)
                     };
                 });
             };
@@ -40,14 +65,17 @@ var getCity = function (cityName) {
         });
 };
 
-var destinationCityWeather = function (lat, lon, name) {
+
+// API to get the lat and lon from getCity() and fetch data from an API to retrieve weather information for the destination city
+var weatherData = function (lat, lon, name) {
     var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=f9dcdf6690d0d22c5198371e258e8bb2";
     fetch(apiURL)
         .then(function (res) {
             if (res.ok) {
                 res.json().then(function (data) {
+                    console.log(`Destination City: ${name}`)
                     console.log(data)
-                    console.log(name)
+
                 });
             };
         })
@@ -56,7 +84,23 @@ var destinationCityWeather = function (lat, lon, name) {
             console.log(error);
             return
         });
-
 };
+
+
+// function to find attractions at destination city. returns 10 natural attractions within a 10k radius of the coordinates of the destination city inputted
+// var attractions = function (lon, lat) {
+//     var apiURL = "https://api.opentripmap.com/0.1/en/places/radius?radius=10000&lon=" + lon + "&lat=" + lat + "&kinds=natural&limit=10&apikey=5ae2e3f221c38a28845f05b69efdf761d15a073a57889029b8209b98"
+
+//     fetch(apiURL)
+//         .then(function (res) {
+//             if (res.ok) {
+//                 res.json().then(function (data) {
+//                     console.log(data)
+//                 })
+//             }
+//         })
+// }
+
+
 
 // display functions (1 for flights, 1 for weather, 1 for hotels/restaurants)

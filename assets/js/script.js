7175a1d2c292b8970a0b6fc9d28669e63eaffbdd
charanvir <charanvir123@gmail.com>
2022-03-31 20:27:23 -0400

    added a flights API

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,8 +34,9 @@ var getCity = function (destinationCity, currentCity) {
                     if (data.length === 0) {
                         alert("Please enter a valid city name")
                     } else {
-                        weatherData(data[0].lat, data[0].lon, data[0].name)
+                        // weatherData(data[0].lat, data[0].lon, data[0].name)
                         // attractions(data[0].lon, data[0].lat)
+                        // restaurants(data[0].lon, data[0].lat)
                     };
                 });
             };
@@ -67,24 +68,25 @@ var getCity = function (destinationCity, currentCity) {
 
 
 // API to get the lat and lon from getCity() and fetch data from an API to retrieve weather information for the destination city
-var weatherData = function (lat, lon, name) {
-    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=f9dcdf6690d0d22c5198371e258e8bb2";
-    fetch(apiURL)
-        .then(function (res) {
-            if (res.ok) {
-                res.json().then(function (data) {
-                    console.log(`Destination City: ${name}`)
-                    console.log(data)
+// var weatherData = function (lat, lon, name) {
+//     var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=f9dcdf6690d0d22c5198371e258e8bb2";
 
-                });
-            };
-        })
-        .catch(function (error) {
-            alert("Please Try Again");
-            console.log(error);
-            return
-        });
-};
+//     fetch(apiURL)
+//         .then(function (res) {
+//             if (res.ok) {
+//                 res.json().then(function (data) {
+//                     console.log(`Destination City: ${name}`)
+//                     console.log(data)
+
+//                 });
+//             };
+//         })
+//         .catch(function (error) {
+//             alert("Please Try Again");
+//             console.log(error);
+//             return
+//         });
+// };
 
 
 // function to find attractions at destination city. returns 10 natural attractions within a 10k radius of the coordinates of the destination city inputted
@@ -99,9 +101,48 @@ var weatherData = function (lat, lon, name) {
 //                 })
 //             }
 //         })
+//         .catch(function (error) {
+//             alert("Please Try Again");
+//             console.log(error);
+//             return
+//         });
 // }
 
-// function to find flights from the current city to the destination city
+// function to find restaurants within a 10 km radius of the destination city coordinates
+// var restaurants = function (lon, lat) {
+//     var apiURL = "https://api.opentripmap.com/0.1/en/places/radius?radius=10000&lon=" + lon + "&lat=" + lat + "&kinds=restaurants&limit=10&apikey=5ae2e3f221c38a28845f05b69efdf761d15a073a57889029b8209b98"
+
+//     fetch(apiURL)
+//         .then(function (res) {
+//             if (res.ok) {
+//                 res.json().then(function (data) {
+//                     console.log(data)
+//                 })
+//             }
+//         })
+//         .catch(function (error) {
+//             alert("Please Try Again");
+//             console.log(error);
+//             return
+//         });
+// }
+
+// function to find flights from current city to destination city
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'skyscanner44.p.rapidapi.com',
+        'X-RapidAPI-Key': 'ac9801f2b6msha969dce2a1a2b53p1c5a9bjsn3bd2a7d8c6fd'
+    }
+};
+
+var flights = function () {
+    fetch('https://skyscanner44.p.rapidapi.com/search?adults=1&origin=MUC&destination=BER&departureDate=2022-06-28&currency=EUR', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+
 
 
 
